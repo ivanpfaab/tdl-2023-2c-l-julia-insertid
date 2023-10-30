68824063a82dac5c689f440844e24e06fdedff68
jdieguez1 <jdieguez@fi.uba.ar>
2023-10-30 20:09:45 -0300

    Añado configuración de proyecto con dependencias de Pluto para notebook.jl

---
+++ notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.31
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 69c5f944-7778-11ee-1473-eff633c169cb
+println("Hola TDL!")
+
+# ╔═╡ Cell order:
+# ╠═69c5f944-7778-11ee-1473-eff633c169cb
